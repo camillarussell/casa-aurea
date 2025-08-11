f817cd61f830f6a9abaa612170724b77d510ba2e
Camilla Russell <milla_russell@hotmail.com>
2025-08-11 15:18:32 -0300

    feat(layout): create Layout component with Header and Footer

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Footer from '../Footer';
+import Header from '../Header';
+import { LayoutProps } from './Layout.types';
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
