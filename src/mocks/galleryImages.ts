ce54641dda89c1580aae2e9dc46e48a3a486a3e6
Camilla Russell <milla_russell@hotmail.com>
2025-08-12 14:06:02 -0300

    chore(gallery): add mock data and gallery image assets

---
+++ src/mocks/galleryImages.ts
@@ -0,0 +1,150 @@
+import sal1 from '../assets/sala-1.webp';
+import sal2 from '../assets/sala-2.webp';
+import sal3 from '../assets/sala-3.webp';
+import sal4 from '../assets/sala-4.webp';
+import sal6 from '../assets/sala-6.webp';
+import sal7 from '../assets/sala-7.webp';
+import ter2 from '../assets/terraco-2.webp';
+import ter4 from '../assets/terraco-4.webp';
+import bwcso1 from '../assets/bwc-social-1.webp';
+import bwcsu1 from '../assets/bwc-suite-1.webp';
+import chu1 from '../assets/churrasqueira-1.webp';
+import gra1 from '../assets/gramado-1.webp';
+import gra2 from '../assets/gramado-2.webp';
+import gra3 from '../assets/gramado-3.webp';
+import gra4 from '../assets/gramado-4.webp';
+import gra5 from '../assets/gramado-5.webp';
+import gra6 from '../assets/gramado-6.webp';
+import gra7 from '../assets/gramado-7.webp';
+import jan1 from '../assets/jantar-1.webp';
+import jan2 from '../assets/jantar-2.webp';
+import jan3 from '../assets/jantar-3.webp';
+import jan4 from '../assets/jantar-4.webp';
+import jan5 from '../assets/jantar-5.webp';
+import jan6 from '../assets/jantar-6.webp';
+import pis1 from '../assets/piscina-1.webp';
+import pis2 from '../assets/piscina-2.webp';
+import pis3 from '../assets/piscina-3.webp';
+import pis4 from '../assets/piscina-4.webp';
+import pis5 from '../assets/piscina-5.webp';
+import lar1 from '../assets/quarto-1-1.webp';
+import lar2 from '../assets/quarto-1-2.webp';
+import lar3 from '../assets/quarto-1-3.webp';
+import lar4 from '../assets/quarto-1-4.webp';
+import ver1 from '../assets/quarto-2-1.webp';
+import ver2 from '../assets/quarto-2-2.webp';
+import ver3 from '../assets/quarto-2-3.webp';
+import ver4 from '../assets/quarto-2-4.webp';
+import ver5 from '../assets/quarto-2-5.webp';
+import azu1 from '../assets/quarto-3-1.webp';
+import azu2 from '../assets/quarto-3-2.webp';
+import beg1 from '../assets/quarto-4-1.webp';
+import beg2 from '../assets/quarto-4-2.webp';
+import coz1 from '../assets/cozinha-1.webp';
+import coz2 from '../assets/cozinha-2.webp';
+
+export const galleryImages = [
+  {
+    location: 'piscina',
+    items: [
+      { id: 'pis-1', src: pis1, alt: 'Foto da Piscina 1' },
+      { id: 'pis-2', src: pis2, alt: 'Foto da Piscina 2' },
+      { id: 'pis-3', src: pis3, alt: 'Foto da Piscina 3' },
+      { id: 'pis-4', src: pis4, alt: 'Foto da Piscina 4' },
+      { id: 'pis-5', src: pis5, alt: 'Foto da Piscina 5' },
+    ],
+  },
+  {
+    location: 'terraco',
+    items: [
+      { id: 'ter-2', src: ter2, alt: 'Foto do terraço 2' },
+      { id: 'ter-4', src: ter4, alt: 'Foto do terraço 4' },
+    ],
+  },
+  {
+    location: 'sala',
+    items: [
+      { id: 'sal-1', src: sal1, alt: 'Foto da sala 1' },
+      { id: 'sal-2', src: sal2, alt: 'Foto da sala 2' },
+      { id: 'sal-3', src: sal3, alt: 'Foto da sala 3' },
+      { id: 'sal-4', src: sal4, alt: 'Foto da sala 4' },
+      { id: 'sal-6', src: sal6, alt: 'Foto da sala 6' },
+      { id: 'sal-7', src: sal7, alt: 'Foto da sala 7' },
+    ],
+  },
+  {
+    location: 'cozinha',
+    items: [
+      { id: 'coz-1', src: coz1, alt: 'Foto da Cozinha 1' },
+      { id: 'coz-2', src: coz2, alt: 'Foto da Cozinha 2' },
+    ],
+  },
+  {
+    location: 'jantar',
+    items: [
+      { id: 'jan-1', src: jan1, alt: 'Foto do Jantar 1' },
+      { id: 'jan-2', src: jan2, alt: 'Foto do Jantar 2' },
+      { id: 'jan-3', src: jan3, alt: 'Foto do Jantar 3' },
+      { id: 'jan-4', src: jan4, alt: 'Foto do Jantar 4' },
+      { id: 'jan-5', src: jan5, alt: 'Foto do Jantar 5' },
+      { id: 'jan-6', src: jan6, alt: 'Foto do Jantar 6' },
+    ],
+  },
+  {
+    location: 'quartoLaranja',
+    items: [
+      { id: 'lar-1', src: lar1, alt: 'Foto do Quarto laranja 1' },
+      { id: 'lar-2', src: lar2, alt: 'Foto do Quarto laranja 2' },
+      { id: 'lar-3', src: lar3, alt: 'Foto do Quarto laranja 3' },
+      { id: 'lar-4', src: lar4, alt: 'Foto do Quarto laranja 4' },
+    ],
+  },
+  {
+    location: 'suiteVerde',
+    items: [
+      { id: 'ver-1', src: ver1, alt: 'Foto da Suíte verde 1' },
+      { id: 'ver-2', src: ver2, alt: 'Foto da Suíte verde 2' },
+      { id: 'ver-3', src: ver3, alt: 'Foto da Suíte verde 3' },
+      { id: 'ver-4', src: ver4, alt: 'Foto da Suíte verde 4' },
+      { id: 'ver-5', src: ver5, alt: 'Foto da Suíte verde 5' },
+    ],
+  },
+  {
+    location: 'quartoAzul',
+    items: [
+      { id: 'azu-1', src: azu1, alt: 'Foto do Quarto Azul 1' },
+      { id: 'azu-2', src: azu2, alt: 'Foto do Quarto Azul 2' },
+    ],
+  },
+  {
+    location: 'quartoBege',
+    items: [
+      { id: 'beg-1', src: beg1, alt: 'Foto do Quarto Bege 1' },
+      { id: 'beg-2', src: beg2, alt: 'Foto do Quarto Bege 2' },
+    ],
+  },
+  {
+    location: 'gramado',
+    items: [
+      { id: 'gra-1', src: gra1, alt: 'Foto do Gramado 1' },
+      { id: 'gra-2', src: gra2, alt: 'Foto do Gramado 2' },
+      { id: 'gra-3', src: gra3, alt: 'Foto do Gramado 3' },
+      { id: 'gra-4', src: gra4, alt: 'Foto do Gramado 4' },
+      { id: 'gra-5', src: gra5, alt: 'Foto do Gramado 5' },
+      { id: 'gra-6', src: gra6, alt: 'Foto do Gramado 6' },
+      { id: 'gra-7', src: gra7, alt: 'Foto do Gramado 7' },
+    ],
+  },
+  {
+    location: 'banheiroSocial',
+    items: [{ id: 'bwcso-1', src: bwcso1, alt: 'Foto do BWC Social' }],
+  },
+  {
+    location: 'banheiroSuitel',
+    items: [{ id: 'bwcsu-1', src: bwcsu1, alt: 'Foto do BWC da Suíte' }],
+  },
+  {
+    location: 'churrasqueira',
+    items: [{ id: 'chu-1', src: chu1, alt: 'Foto da churrasqueira' }],
+  },
+];
